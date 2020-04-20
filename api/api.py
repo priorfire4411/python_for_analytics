81dc4f5bae3046de0549cc8c5e2603d31b1c6981
Brian Craft <craft1.brian@gmail.com>
2020-04-20 15:32:36 -0500

    removed answers

--- api/api.py
+++ api/api.py
@@ -5,7 +5,7 @@ from joblib import load
 import numpy as np
 
 # curl -d "petal_length=2.2" -d "petal_width=5.1" -d "sepal_length=1.2" -d "sepal_width=2.2" http://127.0.0.1:5000/
-# curl -d "petal_length=5" -d "petal_width=1" -d "sepal_length=1.2" -d "sepal_width=2.2" http://127.0.0.1:5000/
+# curl -d "petal_length=10" -d "petal_width=3.5" -d "sepal_length=2" -d "sepal_width=5" http://127.0.0.1:5000/
 
 app = Flask(__name__)
 api = Api(app)
@@ -14,17 +14,27 @@ class getPrediction(Resource):
 	# resource let's you define get and put methods which coincide to http request here we want the
 	def post(self):
 		parser = reqparse.RequestParser()
-		parser.add_argument('petal_length', type = float, required = True)
-		parser.add_argument('petal_width', type = float, required = True)
-		parser.add_argument('sepal_length', type = float, required = True)
-		parser.add_argument('sepal_width', type = float, required = True)
+		#parser.add_argument('petal_length', type = float, required = True)
+		#parser.add_argument('petal_width', type = float, required = True)
+		#parser.add_argument('sepal_length', type = float, required = True)
+		#parser.add_argument('sepal_width', type = float, required = True)
+
+		parser.add_argument('movie', type = float, required = True)
 
 		# dictionary
 		arguments = parser.parse_args()
 
-		model = load("model/irisDecisionTree.joblib") 
-		vector = [arguments["petal_length"], arguments["petal_width"], arguments["sepal_length"], arguments["sepal_width"]]
-		prediction = model.predict([vector]) 
+		predictions = collaborative_filter(arguments["movie"])
+
+		# 1M movvies in your database
+		# compare movie to 1M items 1M calculations
+		# find most similar
+		# return results
+
+
+		#model = load("model/irisDecisionTree.joblib") 
+		#vector = [arguments["petal_length"], arguments["petal_width"], arguments["sepal_length"], arguments["sepal_width"]]
+		#prediction = model.predict([vector]) # numpy array
 
 		# returns an array so need to unpack
 		# to make it flask friendly to return
